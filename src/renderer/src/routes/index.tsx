355e717e0903d246a3518946123c64e52b33c0b8
qianzhu <36325249+syt-honey@users.noreply.github.com>
2023-11-20 17:14:40 +0800

    fix: fix blank page in prod mode

--- src/renderer/src/routes/index.tsx
+++ src/renderer/src/routes/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { routePages } from './route-page'
 
 export const AppRouter: React.FC = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 {Object.keys(routePages).map((name) => {
                     const { component, path } = routePages[name]
                     return <Route key={name} path={path} Component={component} />
                 })}
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
